d31718f1fdfc4e7d224201f8fc632acd9310ef8b
ts7even <trevorpaulseibert@gmail.com>
2022-07-03 18:37:16 -0500

    Added Regression and regression df

--- quant/regressions.py
+++ quant/regressions.py
@@ -13,23 +13,23 @@ df = pd.read_csv('source/data/sp_returns.csv')
 df1 = df.rename(columns={'^GSPC': 'SP50'})
 
 def regression_stats():
-    regress_append = []
+
+    total_df = pd.DataFrame()
 
     for t in tickers:
         model = smf.ols(f'{t} ~ SP50', data=df1).fit()
+        ticker = (f'{t}')
         beta = model.params['SP50']
         alpha = model.params['Intercept']
         std_error = model.bse['Intercept']
 
-        
-        res = pd.DataFrame({f'{t} Beta': beta,
-                            f'{t} Alpha': alpha,
-                            f'{t} ASTDE': std_error
-                            },index=[0])
-        
-        regress_append.append(res)
+        resBeta = pd.DataFrame({f'{t}': beta },index=[0])
+        resAlpha = pd.DataFrame({f'{t}': alpha},index=[0])
+        resStderr = pd.DataFrame({f'{t}': std_error},index=[0])
 
-    total_df = pd.concat(regress_append,axis=1)
+        df_con = pd.concat([resBeta, resAlpha, resStderr],axis=0)
+        total_df = pd.concat([total_df, df_con],axis=1)
+
     total_df.to_csv('source/data/sp_regression.csv')
     print(total_df)
 
