75df81f72f87400cbf6703edef1d37199033484c
ts7even <trevorpaulseibert@gmail.com>
2022-07-10 18:20:43 -0500

    Fixed Assertion Error, Had to correct the for loop using the old name of the regression

--- 1screener/stock_returns.py
+++ 1screener/stock_returns.py
@@ -5,7 +5,7 @@ import yfinance as yf
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 from sp_tickers import tickers
-
+from sp_tickers import tickers2
 
 
 def stock_prices():
@@ -21,19 +21,20 @@ def stock_prices():
         appended_data.append(df)
     
     appended_data = pd.concat(appended_data, axis=1).dropna()
-    appended_data.rename(columns={'^GSPC': 'SP50'}, inplace=True)
+    appended_data.rename(columns={'^GSPC':'SP50'}, inplace=True)
     sp_ret = appended_data.pct_change(1)
     appended_data.to_csv('source/data_screener/sp_prices.csv')
     sp_ret.to_csv('source/data_screener/sp_returns.csv')
     print(sp_ret.round(4))
 
 
+df1 = pd.read_csv('source/data_screener/sp_returns.csv')
 
 def regression_stats():
-    df1 = pd.read_csv('source/data_screener/sp_returns.csv')
+    
     total_df = pd.DataFrame()
 
-    for t in tickers:
+    for t in tickers2:
         model = smf.ols(f'{t} ~ SP50', data=df1).fit()
         beta = model.params['SP50']
         alpha = model.params['Intercept']
@@ -54,5 +55,39 @@ def regression_stats():
     tdf.to_csv('source/data_screener/sp_regression.csv')
     print(tdf.round(5))
 
+
+ 
+def regres_criteria():
+    df1 = pd.read_csv('source/data_screener/sp_regression.csv') # Regression
+    df2 = pd.read_csv('source/data_screener/sp_returns.csv') # Returns
+    df1.rename(columns={ 'Unnamed: 0': 'Ticker'}, inplace=True)
+
+
+    rf = (3.1156)/100
+    cds = (26.90)/10000
+    rfr = (rf-cds)
+    rm = 0.08
+
+    
+    # Appending New Column Calculations
+    esteleai_optimal = (df1['Alpha']/df1['StderrA'])
+    df1.insert(5, 'Alpha Opt', esteleai_optimal)
+
+    sharpe_ratio = ((df1['Alpha']+df1['Beta'] - rfr)/df1['StderrB'])
+    df1.insert(6, 'Sharpe', sharpe_ratio)
+
+    capm = ((rfr + df1['Beta'])*(rm-rfr))
+    df1.insert(7, 'CAPM', capm)
+
+    adj_capm = ((rfr + df1['Alpha'] + df1['Beta'])*(rm-rfr))
+    df1.insert(8, ' ADJ_CAPM', adj_capm)
+
+    # Filtering For Values
+    positive_alpha = df1[(df1['Alpha']>=0.001) & (df1['Sharpe'] >=3)]
+    print(positive_alpha.round(5))
+    df1.to_csv('source/data_screener/Stats_Summary.csv')
+
+  
 # stock_prices()
-regression_stats()  
+# regression_stats()
+regres_criteria()   
--- 1screener/test.py
+++
@@ -1,5 +0,0 @@
-import patsy
-import statsmodels
-
-print(patsy.__version__)
-print(statsmodels.__version__)
