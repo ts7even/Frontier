dd66a9afe6ac27da7cd15ea3c65f828ae543302c
ts7even <trevorpaulseibert@gmail.com>
2022-07-11 20:08:26 -0500

    Fixed Summary Stats where it is more optimized and have random values for weights

--- 2my_portfolio/pt_summary.py
+++ 2my_portfolio/pt_summary.py
@@ -91,74 +91,34 @@ def regres_criteria():
     print(df1)
     
 
+
+
 def portfolio_weights():
-    # Cost of portfolio $3719.90
+    # cost = (3719.90)
+    # Always You have to make sure the index and values are in same order. 
     df1 = pd.read_csv('source/data_portfolio/Stats_Summary.csv')
+    df2 = pd.read_csv('source/data_portfolio/pt_returns.csv')
 
-    cost = (3719.90)
+    returns = df2.mean(numeric_only=True)*52
+    size = len(df1)
+    weights = np.random.dirichlet(np.ones(size))
+    # df1['Weights'] = weights
 
-    aapl_weight = df1[(df1['Ticker']=='AAPL')]
-    aapl_weight = (145.78/3719.90)
-
-    amd_weight = df1[(df1['Ticker']=='AMD')]
-    amd_weight = (236.34/3719.90)
-
-    amzn_weight = df1[(df1['Ticker']=='AMZN')]
-    amzn_weight = (232.58/3719.90)
-
-    bnd_weight = df1[(df1['Ticker']=='BND')]
-    bnd_weight = (225.41/3719.90)
-
-    crwd_weight = df1[(df1['Ticker']=='CRWD')]
-    crwd_weight = (381.56/3719.90)
+    data1 = pd.DataFrame({'Ann Returns': returns, 'Weights': weights})
+    data1.reset_index(drop=True, inplace=True)
     
-    dfac_weight = df1[(df1['Ticker']=='DFAC')]
-    dfac_weight = (238.60/3719.90)
-
-    dfas_weight = df1[(df1['Ticker']=='DFAS')]
-    dfas_weight = (98.06/3719.90)
-
-    dfus_weight = df1[(df1['Ticker']=='DFUS')]
-    dfus_weight = (125.73/3719.90)
-
-    ess_weight = df1[(df1['Ticker']=='ESS')]
-    ess_weight = (267.25/3719.90)
-
-    gs_weight = df1[(df1['Ticker']=='GS')]
-    gs_weight = (295.53/3719.90)
-
-    jpm_weight = df1[(df1['Ticker']=='JPM')]
-    jpm_weight = (227.52/3719.90)
-
-    low_weight = df1[(df1['Ticker']=='LOW')]
-    low_weight = (180.94/3719.90)
-
-    msft_weight = df1[(df1['Ticker']=='MSFT')]
-    msft_weight = (267.51/3719.90)
-
-    vea_weight = df1[(df1['Ticker']=='VEA')]
-    vea_weight = (162.24/3719.90)
-
-    vgk_weight = df1[(df1['Ticker']=='VGK')]
-    vgk_weight = (156.03/3719.90)
-
-    voo_weight = df1[(df1['Ticker']=='VOO')]
-    voo_weight = (356.40/3719.90)
-
-    vwob_weight = df1[(df1['Ticker']=='VWOB')]
-    vwob_weight = (122.42/3719.90)
-
-    sp50_weight = df1[(df1['Ticker']=='SP50')]
-    sp50_weight = (0)
-
-    weights = (aapl_weight, amd_weight, amzn_weight, bnd_weight, crwd_weight, dfac_weight,
-    dfas_weight, dfus_weight, ess_weight, gs_weight, jpm_weight, low_weight, msft_weight, vea_weight, vgk_weight, voo_weight, vwob_weight, sp50_weight)
-    df1.insert(10, 'Weights', weights)
-    df1.to_csv('source/data_portfolio/Stats_Summary.csv')
-    print(df1)
+    df3 = pd.concat([df1,data1], axis=1)
+    df3.to_csv('source/data_portfolio/Stats_Summary.csv')
+    print(df3)
+    print(df3['Weights'].sum())
+    
+   
     
   
 # stock_prices()
 # regression_stats()
 # regres_criteria()   
-# portfolio_weights()
+portfolio_weights()
+
+
+
