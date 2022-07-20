d0c215bbf5554f27bf61ec841b6e016067268d61
ts7even <trevorpaulseibert@gmail.com>
2022-07-19 19:42:19 -0500

    Updated Folder Name and creating effecient frontier for portfolio with scipy and random var

---
+++ 2portfolio/efficient_frontier.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import scipy as spi 
+import yfinance as yf
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import scipy.optimize as minimize
+from pt_tickers import tickers
+from pt_tickers import tickers2
+
+
