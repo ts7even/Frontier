833128aaa1706f4aeb7e4d79b3735dfec47e0f8a
ts7even <trevorpaulseibert@gmail.com>
2022-07-19 21:45:32 -0500

    Renamed Frontier, Also adding a second alternative.

--- 2portfolio/frontier1.py
+++ 2portfolio/frontier1.py
@@ -126,22 +126,6 @@ def display_ef_with_selected(mean_returns, cov_matrix, risk_free_rate):
         print (txt,":","annuaised return",round(an_rt[i],2),", annualised volatility:",round(an_vol[i],2))
     print ("-"*80)
     
-    # fig, ax = plt.subplots(figsize=(10, 7))
-    # ax.scatter(an_vol,an_rt,marker='o',s=200)
-
-    # for i, txt in enumerate(appended_data.columns):
-    #     ax.annotate(txt, (an_vol[i],an_rt[i]), xytext=(10,0), textcoords='offset points')
-    # ax.scatter(sdp,rp,marker='*',color='r',s=500, label='Maximum Sharpe ratio')
-    # ax.scatter(sdp_min,rp_min,marker='*',color='g',s=500, label='Minimum volatility')
-
-    # target = np.linspace(rp_min, 0.34, 50)
-    # efficient_portfolios = efficient_frontier(mean_returns, cov_matrix, target)
-    # ax.plot([p['fun'] for p in efficient_portfolios], target, linestyle='-.', color='black', label='efficient frontier')
-    # ax.set_title('Portfolio Optimization with Individual Stocks')
-    # ax.set_xlabel('annualised volatility')
-    # ax.set_ylabel('annualised returns')
-    # ax.legend(labelspacing=0.8)
-    # plt.show()
 
 display_ef_with_selected(mean_returns, cov_matrix, risk_free_rate)
 
