There is something wrong with scipy optimization with average negative returns

2portfolio/frontier1.py
@@ -49,7 +49,7 @@ def max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate):
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix, risk_free_rate)
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
-    bound = (0.0,0.10)
+    bound = (0.0,1.0)
     bounds = tuple(bound for asset in range(num_assets))
     result = sco.minimize(neg_sharpe_ratio, num_assets*[1./num_assets,], args=args,
                         method='SLSQP', bounds=bounds, constraints=constraints)
@@ -62,7 +62,7 @@ def min_variance(mean_returns, cov_matrix):
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix)
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
-    bound = (0.0,0.10)
+    bound = (0.0,1.0)
     bounds = tuple(bound for asset in range(num_assets))
 
     result = sco.minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args,

2portfolio/frontier3.py
@@ -6,16 +6,28 @@ import scipy.optimize as sco
 from scipy.optimize import minimize
 from pt_tickers import tickers3
 
+# This is the SCIPY Optimization
+
+appended_data = []
+for t in tickers3:
+    stock_info = yf.Ticker(f'{t}').history(period='1y',interval='1d')
+    close = stock_info['Close']
+    df = pd.DataFrame({f'{t}': close})
+    appended_data.append(df)
+appended_data = pd.concat(appended_data, axis=1).dropna()
 
 
+# Statistics
+risk_free_rate = 0.0298
+returns = appended_data.pct_change()
+mean_returns = returns.mean()
+cov_matrix = returns.cov()
 
+# Log Returns Statistics\
+log_return = np.log(appended_data/appended_data.shift(1))
+mean_log_returns = log_return.mean()
+Sigma = log_return.cov()
 
-
-
-
-
-
-# This is the SciPy Optimization
 
 # Efficent Markowiz Frontier with SciPy | Not working right now
 rMin = 0.02