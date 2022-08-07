2fc7218bae82bfe5629f333491b187300f1245dd
ts7even <trevorpaulseibert@gmail.com>
2022-08-06 19:00:41 -0500

    Need to finish ff5 model and add it to dataframe

--- 3factorModels/ff5.py
+++ 3factorModels/ff5.py
@@ -4,6 +4,7 @@ import scipy as spi
 import yfinance as yf
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
+# from sklearn.preprocessing import linear_model
 from pt_tickers import tickers
 from pt_tickers import tickers2
 from pt_tickers import tickers3
@@ -15,13 +16,13 @@ df1 = pd.read_csv('source/multifactor/F-F_Research_Data_5_Factors_2x3_daily.CSV'
 df2 = df1[(df1['Date']>=20170703)]
 df2['Date'] = pd.to_datetime(df2['Date'], format="%Y%m%d")
 
-print(df2)
 
-market_premium = df2['Mkt-RF']
-size_premium = df2['SMB']
-value_premium = df2['HML']
-robust_and_weak_profitability = df2['RMW']
-investment_conservative_minus_agressive = df2['CMA']
+
+market_premium = df2['Mkt-RF'].mean()
+size_premium = df2['SMB'].mean()
+value_premium = df2['HML'].mean()
+robust_and_weak_profitability = df2['RMW'].mean()
+investment_conservative_minus_agressive = df2['CMA'].mean()
 
 appended_data = pd.DataFrame()
 for t in tickers:
@@ -37,12 +38,55 @@ df4 = df2.merge(df3, on='Date')
 
 
 def multiRegression():
+    ff5_df = pd.DataFrame()
     for t in tickers3:
         X = df4[['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA']]
         y = df4[f'{t}']
         X1 = sm.add_constant(X)
         model = sm.OLS(y, X1).fit()
-        print(model.summary(title=f'{t}'))
-
-        
+        int_coef = model.params
+    print(int_coef)
 multiRegression()
+
+
+
+
+
+# def multiRegression():
+#     ff5_df = pd.DataFrame()
+#     for t in tickers3:
+#         X = df4[['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA']]
+#         y = df4[f'{t}']
+#         X1 = sm.add_constant(X)
+#         model = sm.OLS(y, X1).fit()
+#         intercept = model.params['const']
+#         beta_mkrt_rf = model.params['Mkt-RF']
+#         beta_smb = model.params['SMB']
+#         beta_hml = model.params['HML']
+#         beta_rmw = model.params['RMW']
+#         beta_cma = model.params['CMA']
+#         regress_data = pd.DataFrame({
+#             f'{t}':[intercept],
+#             f'{t}':[beta_mkrt_rf],
+#             f'{t}':[beta_smb],
+#             f'{t}':[beta_hml],
+#             f'{t}':[beta_rmw],
+#             f'{t}':[beta_cma]
+#         })
+#         ff5_df = pd.concat([ff5_df, regress_data], axis=1)
+#     tdf = ff5_df.transpose()
+#     tdf.columns = ['Intercept','Mrkt-Rf', 'SMB', 'HML', 'RMW', 'CMA']
+#     print(tdf)
+# multiRegression()
+
+
+# def multiRegression():
+#     ff5_df = pd.DataFrame()
+#     for t in tickers3:
+#         X = df4[['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA']]
+#         y = df4[f'{t}']
+#         X1 = sm.add_constant(X)
+#         model = sm.OLS(y, X1).fit()
+#         print(model.summary(title=f'{t}'))
+        
+# multiRegression()
