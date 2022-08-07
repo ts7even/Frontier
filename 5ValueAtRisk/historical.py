0ae3603f3ba951eadeca51ddc27796cbffae55d9
ts7even <trevorpaulseibert@gmail.com>
2022-08-07 15:54:27 -0500

    Updated Historical VAR

---
+++ 5ValueAtRisk/historical.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+from pandas_datareader import data as pdr
+from scipy.stats import norm, t
+import matplotlib.pyplot as plt
+
+# Import data
+def getData(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start=start, end=end)
+    stockData = stockData['Close']
+    returns = stockData.pct_change
+    meanReturns = returns.mean
+    covMatrix = returns.cov
+    return returns, meanReturns, covMatrix
+
+
+# Portfolio Performance
+def portfolioPerformance(weights, meanReturns, covMatrix, Time):
+    returns = np.sum(meanReturns*weights)*Time
+    std = np.sqrt( np.dot(weights.T, np.dot(covMatrix, weights)) ) * np.sqrt(Time)
+    return returns, std
+stockList = ['CBA', 'BHP', 'TLS', 'NAB', 'WBC', 'STO']
+stocks = [stock+'.AX' for stock in stockList]
+endDate = dt.datetime.now
+startDate = endDate - dt.timedelta(days=800)
+returns, meanReturns, covMatrix = getData(stocks, start=startDate, end=endDate)
+returns = returns.dropna
+weights = np.random.random(len(returns.columns))
+weights /= np.sum(weights)
+returns['portfolio'] = returns.dot(weights)
+
+def historicalVaR(returns, alpha=5):
+    """
+    Read in a pandas dataframe of returns / a pandas series of returns
+    Output the percentile of the distribution at the given alpha confidence level
+    """
+    if isinstance(returns, pd.Series):
+        return np.percentile(returns, alpha)
+    # A passed user-defined-function will be passed a Series for evaluation.
+    elif isinstance(returns, pd.DataFrame):
+        return returns.aggregate(historicalVaR, alpha=alpha)
+    else:
+        raise TypeError("Expected returns to be dataframe or series")
+
+
+def historicalCVaR(returns, alpha=5):
+    """
+    Read in a pandas dataframe of returns / a pandas series of returns
+    Output the CVaR for dataframe / series
+    """
+    if isinstance(returns, pd.Series):
+        belowVaR = returns <= historicalVaR(returns, alpha=alpha)
+        return returns[belowVaR].mean
+    # A passed user-defined-function will be passed a Series for evaluation.
+    elif isinstance(returns, pd.DataFrame):
+        return returns.aggregate(historicalCVaR, alpha=alpha)
+    else:
+        raise TypeError("Expected returns to be dataframe or series")
+# 100 days
+Time = 100
+hVaR = -historicalVaR(returns['portfolio'], alpha=5)*np.sqrt(Time)
+hCVaR = -historicalCVaR(returns['portfolio'], alpha=5)*np.sqrt(Time)
+pRet, pStd = portfolioPerformance(weights, meanReturns, covMatrix, Time)
+InitialInvestment = 10000
+print('Expected Portfolio Return:      ', round(InitialInvestment*pRet,2))
+print('Value at Risk 95th CI    :      ', round(InitialInvestment*hVaR,2))
+print('Conditional VaR 95th CI  :      ', round(InitialInvestment*hCVaR,2))
+
+
+
+
+
