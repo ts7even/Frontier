32f2fd9445f7aef349a1186fdad6fce5bbaae0bb
ts7even <trevorpaulseibert@gmail.com>
2022-08-07 14:51:44 -0500

    Finished Beta version of Fama French 5 Factor Model. Need to Probabily subtract RF from ER of stocks

--- 3factorModels/ff5.py
+++ 3factorModels/ff5.py
@@ -15,15 +15,17 @@ import datetime
 df1 = pd.read_csv('source/multifactor/F-F_Research_Data_5_Factors_2x3_daily.CSV')
 df2 = df1[(df1['Date']>=20170703)]
 df2['Date'] = pd.to_datetime(df2['Date'], format="%Y%m%d")
+print(df2)
 
-
-
+risk_free_rate = df2['RF'].mean()
 market_premium = df2['Mkt-RF'].mean()
 size_premium = df2['SMB'].mean()
 value_premium = df2['HML'].mean()
 robust_and_weak_profitability = df2['RMW'].mean()
 investment_conservative_minus_agressive = df2['CMA'].mean()
 
+
+# For the Returns, Probabily need to calculate the excess returns by subtracting the risk free rate.
 appended_data = pd.DataFrame()
 for t in tickers:
     stock_info = yf.Ticker(f'{t}').history(start='2017-06-30', end='2022-07-01',interval='1d')
@@ -37,7 +39,6 @@ for t in tickers:
 df4 = df2.merge(df3, on='Date')
 
 
-# Proabably need to do a dual for loop to calculate each intercept in params.
 
 
 def multiRegression():
@@ -50,10 +51,16 @@ def multiRegression():
         print(model.summary(title=f'{t}'))
         int_coef = pd.DataFrame({f'{t}':model.params})
         ff5_df = pd.concat([ff5_df, int_coef], axis=1)
-    tran = ff5_df.transpose()
-    print(tran)
-
-multiRegression()
+    ff5_result = ff5_df.transpose()
+    return ff5_result
 
 
+def ff5_expectedReturn():
+    ff5_er = multiRegression()
+    five_factor_er = (((risk_free_rate + ff5_er['Mkt-RF'])*market_premium) + 
+    (ff5_er['SMB']*size_premium) + (ff5_er['HML']*value_premium) + (ff5_er['RMW']*robust_and_weak_profitability) + 
+    (ff5_er['CMA']*investment_conservative_minus_agressive))**(252/(len(df4)))
+    print(five_factor_er)
+
+ff5_expectedReturn()
 
