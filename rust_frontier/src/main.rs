3b02c6ca972877b850ca609c212be04bb9ca67be
ts7even <trevorpaulseibert@gmail.com>
2022-08-19 20:19:30 -0500

    Updated Tickers and Rust

--- rust_frontier/src/main.rs
+++ rust_frontier/src/main.rs
@@ -1,3 +1,20 @@
+use yahoo_finance_api as yahoo;
+//use std::time::{Duration, UNIX_EPOCH};
+//use chrono::{Utc,TimeZone};
+use tokio_test;
+
+mod nasdaq;
 fn main() {
-    println!("Hello, world!");
+    let ticker = nasdaq::stocks;
+    let provider = yahoo::YahooConnector::new();
+    for t in ticker {
+        let response = tokio_test::block_on(provider.get_quote_range(t, "1d", "1mo")).unwrap();
+        let quotes = response.quotes().unwrap();
+        println!("{}", t);
+        for item in quotes
+        {
+            let adj = item.adjclose;
+            println!("{:.2}", adj)    
+    }
+  }
 }
