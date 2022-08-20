519df9fc5e923c63167ab1cd867e3a3b4f079f28
ts7even <trevorpaulseibert@gmail.com>
2022-08-19 23:23:32 -0500

    Added Tickers to Rust but takes 30 mins to complete

--- rust_frontier/src/main.rs
+++ rust_frontier/src/main.rs
@@ -1,14 +1,14 @@
 use yahoo_finance_api as yahoo;
 //use std::time::{Duration, UNIX_EPOCH};
 //use chrono::{Utc,TimeZone};
-use tokio_test;
+// use tokio_test;
 
 mod nasdaq;
 fn main() {
-    let ticker = nasdaq::stocks;
+    let ticker = nasdaq::tickers;
     let provider = yahoo::YahooConnector::new();
     for t in ticker {
-        let response = tokio_test::block_on(provider.get_quote_range(t, "1d", "1mo")).unwrap();
+        let response = provider.get_quote_range(t, "1d", "1mo").unwrap();
         let quotes = response.quotes().unwrap();
         println!("{}", t);
         for item in quotes
