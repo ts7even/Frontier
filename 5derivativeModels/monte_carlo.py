ba654ec1b12e6d500a7eff8b92f77fbc3c76346b
ts7even <trevorpaulseibert@gmail.com>
2022-08-21 22:41:54 -0500

    Added version 1 of monte carlo option simulation

--- 5derivativeModels/monte_carlo.py
+++ 5derivativeModels/monte_carlo.py
@@ -1,3 +1,61 @@
 import pandas as pd 
 import numpy as np 
 import scipy as sp 
+import scipy.stats as stats
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+
+matplotlib.use('Qt5Agg')
+
+# Mock it up in python and replicate it in C++, Java, or Rust. 
+
+
+# initial derivative parameters 
+S = 101.15          #stock price
+K = 98.01           #strike price
+vol = 0.0991        #volatility 
+r = 0.01            #risk-free rate 
+N = 10              #number of time steps
+M = 10000000            #number of simulations
+market_value = 3.86 #market price of option
+T = ((datetime.date(2022,3,17)-datetime.date(2022,1,17)).days+1)/365    #time in years
+print(T)
+
+#precompute constants
+dt = T/N
+nudt = (r - 0.5*vol**2)*dt
+volsdt = vol*np.sqrt(dt)
+lnS = np.log(S)
+# Monte Carlo Method
+Z = np.random.normal(size=(N, M)) 
+delta_lnSt = nudt + volsdt*Z 
+lnSt = lnS + np.cumsum(delta_lnSt, axis=0)
+lnSt = np.concatenate( (np.full(shape=(1, M), fill_value=lnS), lnSt ) )
+# Compute Expectation and SE
+ST = np.exp(lnSt)
+CT = np.maximum(0, ST - K)
+C0 = np.exp(-r*T)*np.sum(CT[-1])/M
+sigma = np.sqrt( np.sum( (CT[-1] - C0)**2) / (M-1) )
+SE = sigma/np.sqrt(M)
+print("Call value is ${0} with SE +/- {1}".format(np.round(C0,2),np.round(SE,2)))
+
+
+
+x1 = np.linspace(C0-3*SE, C0-1*SE, 100)
+x2 = np.linspace(C0-1*SE, C0+1*SE, 100)
+x3 = np.linspace(C0+1*SE, C0+3*SE, 100)
+s1 = stats.norm.pdf(x1, C0, SE)
+s2 = stats.norm.pdf(x2, C0, SE)
+s3 = stats.norm.pdf(x3, C0, SE)
+plt.fill_between(x1, s1, color='tab:blue',label='> StDev')
+plt.fill_between(x2, s2, color='cornflowerblue',label='1 StDev')
+plt.fill_between(x3, s3, color='tab:blue')
+plt.plot([C0,C0],[0, max(s2)*1.1], 'k',
+        label='Theoretical Value')
+plt.plot([market_value,market_value],[0, max(s2)*1.1], 'r',
+        label='Market Value')
+plt.ylabel("Probability")
+plt.xlabel("Option Price")
+plt.legend()
+plt.show()
